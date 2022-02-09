c533c7eeec3206aecad2f1acba35fb3ad5bf32fe
typeluis <typeluis@gmail.com>
2022-02-09 17:12:47 -0500

    fixing newegg internal server on heroku

--- newEgg.py
+++ newEgg.py
@@ -12,83 +12,91 @@ new_egg = Blueprint("newEgg", __name__)
 @new_egg.route("/newegg/<string:product>", methods=["GET"])
 def find_newegg_product(product):
 
+    try:
 
-    if request.args.get("page"):
-        #  http://localhost:5000/newegg/am4?page=2
-        url = f"https://www.newegg.com/p/pl?d={product}&N=4131&page={request.args.get('page')}"
-    else:
-        #  http://localhost:5000/newegg/am4
-        url = f"https://www.newegg.com/p/pl?d={product}&N=4131"
+        if request.args.get("page"):
+            #  http://localhost:5000/newegg/am4?page=2
+            url = f"https://www.newegg.com/p/pl?d={product}&N=4131&page={request.args.get('page')}"
+        else:
+            #  http://localhost:5000/newegg/am4
+            url = f"https://www.newegg.com/p/pl?d={product}&N=4131"
 
 
-    session = HTMLSession()
-    page = session.get(url).content
+        session = HTMLSession()
+        page = session.get(url).content
 
-    # page = requests.get(url).text
+        # page = requests.get(url).text
 
-    doc = bs(page, "html.parser")
+        doc = bs(page, "html.parser")
 
-    # finds the pagination element
-    page_text = doc.find('span', class_="list-tool-pagination-text").strong
+        # finds the pagination element
+        page_text = doc.find('span', class_="list-tool-pagination-text").strong
 
-    # pages = int(doc.find_all(class_='btn-group-cell')[-2].button.text)
+        # pages = int(doc.find_all(class_='btn-group-cell')[-2].button.text)
 
-    # splits to get the last page number
-    pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
+        # splits to get the last page number
+        pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
 
-    items_found = {}
+        items_found = {}
 
-    div = doc.find(
-        class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell"
-    )
+        div = doc.find(
+            class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell"
+        )
 
-    # finds all element that has the same text as the product
+        # finds all element that has the same text as the product
 
-    # re.compile(product, re.I) finds element even if it's case sensative or has more characters inside the text element
-    items = div.find_all(text=re.compile(str(product), re.I))
+        # re.compile(product, re.I) finds element even if it's case sensative or has more characters inside the text element
+        items = div.find_all(text=re.compile(str(product), re.I))
 
-    for item in items:
-        parent = item.parent  # the parent of the item element is an <a> tag
-        if parent.name != "a":
-            continue
+        for item in items:
+            parent = item.parent  # the parent of the item element is an <a> tag
+            if parent.name != "a":
+                continue
 
-        link = parent["href"]
+            link = parent["href"]
 
-        short_link = link.split("?")[0].split("/")[-1]
+            short_link = link.split("?")[0].split("/")[-1]
 
-        product_div = item.find_parent(class_="item-container")
+            product_div = item.find_parent(class_="item-container")
 
-        try:
-            price = product_div.find(class_="price-current").strong.string
-            image = product_div.img["src"]
-            # print(price)
-            items_found[item] = {
-                "price": int(price.replace(",", "")),
-                "link": link,
-                "image": image,
-                "short_link": short_link,
+            try:
+                price = product_div.find(class_="price-current").strong.string
+                image = product_div.img["src"]
+                # print(price)
+                items_found[item] = {
+                    "price": int(price.replace(",", "")),
+                    "link": link,
+                    "image": image,
+                    "short_link": short_link,
+                }
+            except:
+                pass
+
+        sorted_items = sorted(items_found.items(), key=lambda x: x[1]["price"])
+
+        # print("LHUIEHUIFHUI", items_found.items())
+
+        sorted_list = []
+        for item in sorted_items:
+            sorted_dict = {
+                "name": item[0],
+                "price": str(item[1]["price"]),
+                "link": item[1]["link"],
+                "image": item[1]["image"],
+                "short_link": item[1]["short_link"],
             }
-        except:
-            pass
 
-    sorted_items = sorted(items_found.items(), key=lambda x: x[1]["price"])
+            sorted_list.append(sorted_dict)
+        # print(sorted_list)
 
-    # print("LHUIEHUIFHUI", items_found.items())
+        return {f"products": sorted_list, "pages": pages}
 
-    sorted_list = []
-    for item in sorted_items:
-        sorted_dict = {
-            "name": item[0],
-            "price": str(item[1]["price"]),
-            "link": item[1]["link"],
-            "image": item[1]["image"],
-            "short_link": item[1]["short_link"],
-        }
+    except Exception as e:
+        print(e)
+        return {"error" f"{e}"}, 400
 
-        sorted_list.append(sorted_dict)
-    # print(sorted_list)
 
-    return {f"products": sorted_list, "pages": pages}
+
 
 
 @new_egg.route("/newegg/page/<string:url>", methods=["GET"])
