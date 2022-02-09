376143922b49b214ddb97952d7bce44009549aa2
typeluis <typeluis@gmail.com>
2022-02-09 04:56:06 -0500

    adding stripe5 test

--- application.py
+++ application.py
@@ -111,8 +111,8 @@ def create_checkout_session():
         session = stripe.checkout.Session.create(
             line_items=cart_list, 
             mode='payment',
-            success_url='http://localhost:3001/success',
-            cancel_url='http://localhost:3001/cancel',
+            success_url='http://localhost:3001/stripe?success',
+            cancel_url='http://localhost:3001/stripe?cancel',
         )
 
         print(session)
